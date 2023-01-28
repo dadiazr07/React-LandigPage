79d4c67b15706786084fd383813afbc8fb642d97
dadiazr07 <dadiazr.07@gmail.com>
2023-01-28 00:26:57 +0000

    Almost ready, need to check vh-100 for the footer

---
+++ src/js/component/Cards.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const Cards = () =>{
+    return (
+        <div className="container mb-auto">
+        <div className="row">
+  <div className="col-3 p-1">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-3 p-1">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-3 p-1">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div className="col-3 p-1">
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+    )
+}
